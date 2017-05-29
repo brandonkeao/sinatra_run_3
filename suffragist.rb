d6804079fda7c9b57cfc56410dba5dd25884f539
brandonkeao <brandon.keao@gmail.com>
2017-05-28 21:03:09 -0600

    Implemented YAML storage and connected all the routes for completed UX

--- suffragist.rb
+++ suffragist.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'yaml/store'
 
 get '/' do
   @title = 'Welcome to the Suffragist!'
@@ -6,9 +7,22 @@ get '/' do
 end
 
 post '/cast' do
-  @title = 'Thanks for casing your vote!'
-  @vote = params['vote']
+  @title = 'Thanks for casting your vote!'
+  @vote  = params['vote']
+  @store = YAML::Store.new 'votes.yml'
+  @store.transaction do
+    @store['votes'] ||= {}
+    @store['votes'][@vote] ||= 0
+    @store['votes'][@vote] += 1
+  end
   erb :cast
+end
+
+get '/results' do
+  @title = 'Results so far:'
+  @store = YAML::Store.new 'votes.yml'
+  @votes = @store.transaction { @store['votes'] }
+  erb :results
 end
 
 Choices = {
@@ -16,4 +30,6 @@ Choices = {
   'PIZ' => 'Pizza',
   'CUR' => 'Curry',
   'NOO' => 'Noodles',
+  'YUM' => 'Chocolate Ice Cream',
+  'ARR' => 'Arroz Con Pollo' 
 }
